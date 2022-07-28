d9dc8daa6f37c5ade6c06aa29a0b29613778d960
SungJaeYu <ysj8781@gmail.com>
2022-07-28 22:50:11 +0900

    done but.. not check

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import slack_bot from slackbot
-import TOKEN from secret
+import slackbot as sb
+import secret
 
 def main():
     week = int(input())
-    bot = slack_bot(TOKEN, week)
-    bot.check_assignment
+    bot = sb.slack_bot(secret.TOKEN, week)
+    bot.check_assignment()
 
 if __name__ == '__main__':
     main()
--- slackbot.py
+++ slackbot.py
@@ -2,40 +2,38 @@ from slack_sdk import WebClient
 import baekjun as bkj
 from datetime import date, datetime, timedelta
 
-from secret import USERS, NAMES, PROBLEMS
+import secret as sc
 
 class slack_bot:
 
     def __init__(self, token, week):
         self.client = WebClient(token)
-        self.done_date = date(2022, 8, 2)
         self.week = week
 
     def send_message(self, channel, text):
-        response = self.client.web_client.chat_postMessage(
+        response = self.client.chat_postMessage(
                 channel=channel,
                 text=text
                 )
     
     def check_assignment_user(self, user, problem_id):
-        solved_problem = bkj.get_solved(user)
+        _, solved_problem = bkj.get_solved(user)
         for problem in solved_problem:
             if problem.get("problemId") == problem_id:
                 return True
         return False
 
     def check_assignment(self):
-        problem_id = PROBLEMS[self.week]
+        problem_id = sc.PROBLEMS[self.week - 1]
         done_list = []
-        for i, user in enumerate(USERS):
+        for i, user in enumerate(sc.USERS):
             done = self.check_assignment_user(user, problem_id)
             if done == False:
-                done_list.append(NAMES[i] + "Fail")
+                done_list.append(sc.NAMES[i] + " : Fail")
             else:
-                done_list.append(NAMES[i] + "Success")
+                done_list.append(sc.NAMES[i] + " : Success")
         
-        channel = "#" + self.week + "주차"
+        channel = "#" + str(self.week) + "주차"
         for check_noti in done_list:
             self.send_message(channel, check_noti)
-        # TODO : 연속된 한개의 메시지로 출력하도록 수정
 
